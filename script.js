bb0a15d1f36cfceb82ddee81cd93b8d08e3aad48
Michael <GrantM1@users.noreply.github.com>
2020-04-08 17:27:02 +0200

    Finished small project musix web songle page site
    worked with styles jquery + jquery owl carousel flex-box styles side-bar menu, buttons etc

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 $(document).ready(function() {
+ // side btn
  $('.side-btn').click(function(){
   $('.side-nav-links').slideToggle(1000);
   $('.side-btn').toggleClass('change')
  })
+ // nav btn
+ $('.nav-btn').click(function(){
+  $('.side-nav').toggleClass('margin');
+  $('.nav-btn').toggleClass('change');
+  $('main').toggleClass('max-width');
+ })
+// owl carousel
+ $('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  smartSpeed: 1000,
+  autoplay: true,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:4
+      }
+  }
+})
 })
